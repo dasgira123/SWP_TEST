9e9e53d19bc6ee40bd7a0a008aa7c7a5c69ee4aa
Vũ Danh Hiếu <vudanhhieu09@gmail.com>
2025-01-11 14:09:53 +0700

    change
    Signed-off-by: Vũ Danh Hiếu <vudanhhieu09@gmail.com>

---
+++ src/java/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 3;
+        b = 5;
+        c = a + b;
+        System.out.println(c);
+    }
+}
